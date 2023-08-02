5eb941c6ccfb2ad4cb61d3240912d36048947b53
Hans Nesbitt <hansnesb@amazon.com>
2023-08-02 13:28:48 -0700

    removed unneeded line of code

--- infra/app.py
+++ infra/app.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-
 import aws_cdk as cdk
-import cdk_nag
 from infra.infra_stack import InfraStack
 
 app = cdk.App()
 InfraStack(app, "infra")
 
-# cdk.Aspects.of(app).add(cdk_nag.AwsSolutionsChecks())
 app.synth()
